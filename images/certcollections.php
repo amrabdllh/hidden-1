83e182c663ab6f0b49b8e9e0dfdad69f0886b9c1
Ammar Abdullah Al-Zahid <56418817+amrabdllh@users.noreply.github.com>
2023-01-04 22:22:27 +0700

    Create certcollections.php

---
+++ images/certcollections.php
@@ -0,0 +1,139 @@
+<!DOCTYPE html>
+<html lang="en-US" dir="ltr">
+	<head>
+<script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-2480891282368590"
+     crossorigin="anonymous"></script>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<meta property="og:site_name" content="IT Certification Forum">
+<meta property="og:locale" content="en_US">
+<meta name="keywords" content="IT Certification Forum">
+<meta name="theme-color" content="#121418">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.css" integrity="sha512-5A8nwdMOWrSz20fDsjczgUidUBR8liPYU+WymTZP1lmY9G6Oc7HlZv156XqnsgNUzTyMefFTcsFH/tnJE/+xBg==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+<link rel="stylesheet" href="<?=base_url('/lib/assets/css/framework.css');?>" media="all">
+<link rel="stylesheet" href="<?=base_url('/lib/assets/css/responsive.css');?>" media="all">
+<link rel="stylesheet" href="<?=base_url('/lib/assets/css/core.css');?>" media="all">
+<link rel="stylesheet" href="<?=base_url('/lib/assets/css/core_responsive.css');?>" media="all">
+<link rel="stylesheet" href="<?=base_url('/lib/assets/css/typicons.css');?>" media="all">
+<link rel="stylesheet" href="<?=base_url('/lib/assets/css/modern_tags.css');?>" media="all">
+<link rel="stylesheet" href="<?=base_url('/lib/assets/css/jqcloud.css');?>" media="all"> 
+<link rel="stylesheet" href="<?=base_url('/lib/assets/css/page.css');?>" media="all">
+<link rel="stylesheet" href="<?=base_url('/lib/assets/css/custom.css');?>" media="all">
+
+
+<!-- <link rel="stylesheet" href="<?=base_url('/lib/assets/css/banner-styles.css');?>" media="all">
+<link rel="stylesheet" href="<?=base_url('/lib/assets/css/free.min.css');?>" media="all">
+<link rel="stylesheet" href="<?=base_url('/lib/assets/css/free-v4-font-face.min.css');?>" media="all">
+<link rel="stylesheet" href="<?=base_url('/lib/assets/css/free-v4-shims.min.css');?>" media="all">
+<link rel="stylesheet" href="<?=base_url('/lib/assets/css/iconochive.css');?>" media="all">
+ -->
+
+ <style>
+ .navbarips{
+    margin-top: 24px;
+    list-style: none;
+    position: absolute;
+    width: 100%;
+    padding: 20px 40px;
+    z-index: 1111111111;
+ }
+ .navbarips li{
+   border-bottom: 1px solid;
+   margin: 5px 0px;
+ }
+ #elSearch {
+    display: block;
+  }
+  #barbtn:checked ~ #smallicons{
+      display:block !important;
+  }
+  .navbarips{
+      background: #303137;
+  }
+  .navbarips li{
+      padding: 5px 0px;
+  }
+  #smallicons, #nabbarbtn, #barbtn{
+  float: left;
+  display:none;}
+  #nabbarbtn button{
+      border: 1px solid #696a6a;
+    background: none;
+    border-radius: 2px;
+  }
+ @media(max-width: 760px) {
+     #nabbarbtn{
+         display:block !important;
+     }
+  
+}
+ .bg-dark{background-color: #121418;}
+    .d-flex{display: flex;}
+	 .text-white{  color:#ffff;}
+	 .text-danger{  color:red;}
+	 .p-0{padding:0px !important;}
+	 .m-0{margin:0px !important;}
+	 .pb-10{ padding-bottom: 10px !important;}
+	 .pb-20{ padding-bottom: 20px !important;}
+	 .pt-10{ padding-top: 10px !important;}
+	 .pt-20{ padding-top: 20px !important;}
+	 .px-10{
+	     padding-right:10px;
+	     padding-left:10px;
+	 }
+	 .px-20{
+	     padding-right:20px;
+	     padding-left:20px;
+	 }
+	 .p-30{padding:30px !important;}
+	 .p-20{padding:20px !important;}
+	 .p-10{padding:10px !important;}
+	 .mt-10{ margin-top: 10px !important;}
+	 .mr-10{ margin-right: 10px !important;}
+	 .mt-20{ margin-top: 20px !important;}
+	 .mb-10{ margin-bottom: 10px !important;}
+	 .mb-20{ margin-bottom: 20px !important;}
+	 .m-10{ margin: 10px !important;}
+	 .h5{font-size: 20px !important;}
+	 .h4{font-size: 25px !important;}
+	 .h3{font-size: 30px !important;}
+	 .bb-1{border-bottom: 1px solid;}
+	 .br-1{border-right: 1px solid;}
+	 .border-1{border: 1px solid;}
+	 .strNewTopicBtn{
+		float: right;
+		border: 1px solid;
+		padding: 3px 10px;
+		border-radius: 5px;
+		background: #477ef7;
+		font-weight: 500;
+		text-align:center;
+	 }
+	 .w-100{width:100%;}
+	 .scrollBox{
+	     overflow: auto;
+        display: block;
+        padding-right: 10px;
+	 }
+	 .form-control{
+	     border: 0px;
+        padding: 10px;
+        margin: 7px 0px;
+	 }
+	.scrollBox::-webkit-scrollbar {width: 7px;}
+	.text-left{text-align:left !important;}
+	.text-center{text-align:center !important;}
+	.btn{
+	    background: none;
+        border: 1px solid;
+        border-radius: 4px;
+        padding: 4px 8px;
+        cursor:pointer;
+	}
+	.btn-orange{
+	    background: #e58400;
+        border-color: #ffb100;
+	}
+ </style>
+
+
