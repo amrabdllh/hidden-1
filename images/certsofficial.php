aa02a06c6e6585f931cd4818ce7c2170473f46c9
Ammar Abdullah Al-Zahid <56418817+amrabdllh@users.noreply.github.com>
2023-01-05 02:58:23 +0700

    Create certsofficial.php

---
+++ images/certsofficial.php
@@ -0,0 +1,60 @@
+<!doctype html>
+<html lang="en">
+<head>
+<script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-7148300905293580"
+     crossorigin="anonymous"></script>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <link rel="shortcut icon" type="image/jpg" href="https://www.certsofficial.com/uploads/favicon.png"/>
+
+
+
+    <?php // Include assets
+        echo link_tag('lib/bootstrap/css/bootstrap.min.css');
+        echo link_tag('lib/font-awesome/css/font-awesome.min.css');
+        echo link_tag('lib/easy-responsive-tabs-to-accordion/css/easy-responsive-tabs.css');
+        echo link_tag('lib/csassets/css/main.css');
+        echo link_tag('lib/scrollbar/jquery.mcustomscrollbar.min.css');
+        echo link_tag('lib/main.css');
+        echo link_tag('lib/custom.css');
+
+        
+        
+    ?> <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-7148300905293580"
+     crossorigin="anonymous"></script>
+    <script type="text/javascript" src="<?php echo base_url('/lib/jquery.min.js'); ?>"></script>
+     
+  
+    <link href="https://fonts.googleapis.com/css?family=Poppins" rel="stylesheet">
+
+
+    
+    
+    
+<!-- Global site tag (gtag.js) - Google Analytics -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-QV3SPGEZKC"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-QV3SPGEZKC');
+</script>
+
+<!--Start of Tawk.to Script-->
+<script type="text/javascript">
+var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
+(function(){
+var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
+s1.async=true;
+s1.src='https://embed.tawk.to/5cac658d557d5f68515ba479/default';
+s1.charset='UTF-8';
+s1.setAttribute('crossorigin','*');
+s0.parentNode.insertBefore(s1,s0);
+})();
+</script>
+<!--End of Tawk.to Script-->
+<script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-7148300905293580"
+     crossorigin="anonymous"></script>
+</head>
+<body class="<?php echo $class ?>">
