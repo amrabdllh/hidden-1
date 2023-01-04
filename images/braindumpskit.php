58738e76a85a86901637cf0e9f9cb9cd777926cd
Ammar Abdullah Al-Zahid <56418817+amrabdllh@users.noreply.github.com>
2023-01-04 22:00:18 +0700

    Create braindumpskit.php

---
+++ images/braindumpskit.php
@@ -0,0 +1,56 @@
+<!doctype html>
+<html lang="en" amp>
+<head>
+<script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-2480891282368590"
+     crossorigin="anonymous"></script>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    		<!--<link rel="shortcut icon" type="image/jpg" href="<?=base_url('uploads/authenticimg/favicon.png');?>"/>-->
+    		<link rel="shortcut icon" type="image/jpg" href="<?=base_url('uploads/images/favicon.png');?>"/>
+
+   
+<link rel="stylesheet" href="<?=base_url('lib/kitassets/css/style.css');?>"/>
+<link rel="stylesheet" href="<?=base_url('lib/kitassets/css/bootstrap.min.css');?>"/>
+
+    <style>
+    .pb-0{
+        padding-bottom: 0px !important;
+    }
+    .footerp{
+        color: #e8e8e8;
+    }
+    body{
+        overflow-x:hidden;
+    }
+       li a:hover{
+           cursor:pointer;
+           color:blue !important;
+       }
+       .-nmbrOfQues{
+           background: #3569b9;
+           color:white !important;
+       }
+       .updateBlockDiv:hover .-nmbrOfQues{
+           background: hsl(107, 75%, 39%);
+           color:white;
+       }
+       .updateBlockDiv:hover .updateDate{
+           background: #3569b9;
+           color:white !important;
+       }
+        .updateBlockDiv:hover .upadateLnk{
+           background: #3569b9 !important;
+           color:white !important;
+       }
+       .first .dropdown-menu .content .Col1 a:hover{
+           cursor:pointer;
+           color:#3569b9 !important;
+           text-decoration: underline !important;
+       }
+   </style>
+   
+   
+
+
+
+
