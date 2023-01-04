b9c005834c37e915561b34d86eaf363fa0d5a37e
Ammar Abdullah Al-Zahid <56418817+amrabdllh@users.noreply.github.com>
2023-01-05 02:42:44 +0700

    Create authenticcerts.php

---
+++ images/authenticcerts.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="theme-color" content="#317EFB"/>
+    <link rel="shortcut icon" type="image/jpg" href="<?php echo base_url('uploads/favicon.jpg') ?>"/>
+
+   
+     <style>
+        #intro {
+              background: url("<?php echo base_url('uploads/e4simages/intro-bg.png') ?>") center bottom no-repeat;
+        }
+        @font-face {
+              font-family: 'Pacifico';
+              font-style: normal;
+              font-weight: 400;
+              src: local('Pacifico Regular'), local('Pacifico-Regular'), url(https://fonts.gstatic.com/s/pacifico/v12/FwZY7-Qmy14u9lezJ-6H6MmBp0u-.woff2) format('woff2');
+              font-display: swap;
+        }
+    </style> 
+<!-- Global site tag (gtag.js) - Google Analytics -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=UA-165868247-1"></script> <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-2480891282368590"
+     crossorigin="anonymous"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'UA-165868247-1');
+</script>
+<!--Start of Tawk.to Script-->
+<script type="text/javascript">
+var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
+(function(){
+var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
+s1.async=true;
+s1.src='https://embed.tawk.to/5cac658d557d5f68515ba479/default';
+s1.charset='UTF-8';
+s1.setAttribute('crossorigin','*');
+s0.parentNode.insertBefore(s1,s0);
+})();
+</script>
+<script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-2480891282368590"
+     crossorigin="anonymous"></script>
+<!--End of Tawk.to Script-->
