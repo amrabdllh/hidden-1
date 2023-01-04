d052039f3929b916e9a7e5146258b2fa55c137f4
Ammar Abdullah Al-Zahid <56418817+amrabdllh@users.noreply.github.com>
2023-01-05 02:47:26 +0700

    Create exams4success.php

---
+++ images/exams4success.php
@@ -0,0 +1,54 @@
+<!doctype html>
+<html lang="en">
+<head>
+<script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-2480891282368590"
+     crossorigin="anonymous"></script>
+    <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <link rel="shortcut icon" type="image/png" href="https://www.exams4success.com/uploads/favicon.png"/>
+    
+
+    <?php // Include assets
+        echo link_tag('/lib/bootstrap/css/bootstrap.min.css');
+        echo link_tag('/lib/font-awesome/css/font-awesome.min.css');
+        echo link_tag('/lib/easy-responsive-tabs-to-accordion/css/easy-responsive-tabs.css');
+      
+        echo link_tag('/lib/owl-carousal/assets/owl.carousel.min.css');
+        echo link_tag('/lib/owl-carousal/assets/owl.theme.green.min.css');
+        echo link_tag('/lib/scrollbar/jquery.mcustomscrollbar.min.css');
+        echo link_tag('/lib/main.css');
+        echo link_tag('/lib/custom.css');
+        
+    ?>
+    <script type="text/javascript" src="<?php echo base_url('/lib/jquery.min.js'); ?>"></script> <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-2480891282368590"
+     crossorigin="anonymous"></script>
+     
+     
+    <link href="https://fonts.googleapis.com/css?family=Poppins" rel="stylesheet">
+    <link href="http://www.exams4success.com/lib/scrollbar/jquery.mcustomscrollbar.min.css" rel="stylesheet">
+<script src="https://use.fontawesome.com/5a8a7bb461.js"></script>
+    
+    
+<!-- Global site tag (gtag.js) - Google Analytics -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=UA-124249404-1"></script> 
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'UA-124249404-1');
+
+     crossorigin="anonymous"></script>
+
+<!--Start of Tawk.to Script--> 
+<script type="text/javascript"> 
+var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
+(function(){
+var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
+s1.async=true;
+s1.src='https://embed.tawk.to/5cac658d557d5f68515ba479/default';
+s1.charset='UTF-8';
+s1.setAttribute('crossorigin','*');
+s0.parentNode.insertBefore(s1,s0);
+})();
+</script>
+
