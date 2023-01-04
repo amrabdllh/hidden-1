f9a154c54285ad32f1e9579a6109fba115fb3d3c
Hikigaya <56418817+amrabdllh@users.noreply.github.com>
2023-01-05 03:29:27 +0700

    Create sadakaam.php

---
+++ images/sadakaam.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-7148300905293580"
+     crossorigin="anonymous"></script>
+	<!-- set the encoding of your site -->
+	<meta charset="utf-8">
+	<!-- set the viewport width and initial-scale on mobile devices -->
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<!-- set the HandheldFriendly -->
+	<meta name="HandheldFriendly" content="True">
+	<!-- include the site stylesheet -->
+	<link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,600%7CPoppins:300,400,500,600,900%7CLily+Script+One" rel="stylesheet">
+	<!-- include the site stylesheet -->
+	<link rel="stylesheet" href="<?= base_url('lib/assets/')?>/css/font-awesome.css">
+	<!-- include the site stylesheet -->
+	<link rel="stylesheet" href="<?= base_url('lib/assets/')?>/css/bootstrap.css">
+	<!-- include the site stylesheet -->
+	<link rel="stylesheet" href="<?= base_url('lib/assets/')?>/css/plugins.css">
+	<!-- include the site stylesheet -->
+	<link rel="stylesheet" href="<?= base_url('lib/assets/')?>/css/icofont.css">
+	<!-- include the site stylesheet -->
+	<link rel="stylesheet" href="<?= base_url('lib/assets/')?>/style.css">
+	<!-- include the site stylesheet -->
+	<link rel="stylesheet" href="<?= base_url('lib/assets/')?>/css/colors.css">
+	<!-- include the site stylesheet -->
+	<link rel="stylesheet" href="<?= base_url('lib/assets/')?>/css/responsive.css">
+	<!-- include the site stylesheet -->
+	<style class="color_css"></style>
+
+  
