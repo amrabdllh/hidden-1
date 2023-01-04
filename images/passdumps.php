7e581ca38ff87a4bbe5c001baff4d72336bfc92b
Hikigaya <56418817+amrabdllh@users.noreply.github.com>
2023-01-05 03:19:49 +0700

    Create passdumps.php

---
+++ images/passdumps.php
@@ -0,0 +1,32 @@
+<!doctype html>
+<html lang="en">
+<head>
+<script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-7148300905293580"
+     crossorigin="anonymous"></script>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, minimum-scale=0.25, maximum-scale=5, initial-scale=1.0">
+    	<link rel="shortcut icon" type="image/jpg" href="/uploads/favicon.jpg"/>
+<link rel="stylesheet" href="https://pro.fontawesome.com/releases/v5.10.0/css/all.css" integrity="sha384-AYmEC3Yw5cVb3ZcuHtOA93w35dYTsvhLPVnYs9eStHfGJvOvKxVfELGroGkvsg+p" crossorigin="anonymous"/>
+     
+    <?php // Include assets
+
+        echo link_tag('lib/assets/css/style.css');
+        echo link_tag('lib/bootstrap/css/bootstrap.min.css');
+        echo link_tag('lib/assets/css/bootstrap.css');
+        echo link_tag('lib/css/select2.min.css');
+    ?>
+    
+
+<style>
+    .navbar-nav .nav-item a.nav-link{
+        color:black;
+    }
+</style>
+
+    
+   
+   
+
+
+
+
