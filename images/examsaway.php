0a297b014e5efe96f06388aea076cc2e89c2396d
Hikigaya <56418817+amrabdllh@users.noreply.github.com>
2023-01-05 03:07:11 +0700

    Create examsaway.php

---
+++ images/examsaway.php
@@ -0,0 +1,35 @@
+<!Doctype HTML>
+<html lang="en">
+<head>
+<script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-7148300905293580"
+     crossorigin="anonymous"></script>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, minimum-scale=0.25, maximum-scale=5, initial-scale=1.0">
+    <link rel="shortcut icon" type="image/jpg" href="/uploads/favicon.jpg"/>
+    <link rel="apple-touch-icon" sizes="76x76" href="/assets/img/apple-icon.png">
+    <!--<link rel="stylesheet" href="https://pro.fontawesome.com/releases/v5.10.0/css/all.css" integrity="sha384-AYmEC3Yw5cVb3ZcuHtOA93w35dYTsvhLPVnYs9eStHfGJvOvKxVfELGroGkvsg+p" crossorigin="anonymous"/>-->
+     <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,600,700" rel="stylesheet" />
+     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+    <?php // Include assets
+        echo link_tag('lib/eawassets/site/css/bootstrap.min.css');
+        echo link_tag('lib/eawassets/site/css/style.css');
+        // echo link_tag('/lib/assets/css/bootstrap.css');
+    ?>
+<script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-7148300905293580"
+     crossorigin="anonymous"></script>
+    <!-- Global site tag (gtag.js) - Google Analytics -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=UA-151211592-1"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'UA-151211592-1');
+</script>
+<script src="https://www.googleoptimize.com/optimize.js?id=OPT-PGMWR4S"></script>
+   
+   
+
+
+
+
